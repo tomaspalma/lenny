2c4a0f8495e81bb198fa4830b2d7d356629ece73
Tomás Palma <tomaspalmaprogramador@gmail.com>
2022-08-28 19:54:51 +0100

    added lazy static evaluation to improve performance in regexes

--- src/regex_validation.rs
+++ src/regex_validation.rs
@@ -1,9 +1,18 @@
 use regex::Regex;
+use lazy_static::lazy_static;
 
 fn is_config_name(candidate: &str) -> bool {
-    Regex::new(r"^[?]$").unwrap().is_match(candidate)
+    lazy_static! {
+         static ref CURRENT_REGEX: Regex = Regex::new(r"^[?]$").unwrap();
+    }
+
+    CURRENT_REGEX.is_match(candidate)
 }
 
 fn is_documentation_specifier(candidate: &str) -> bool {
-    Regex::new(r"^$").unwrap().is_match(candidate)
+    lazy_static! {
+       static ref CURRENT_REGEX: Regex = Regex::new(r"^$").unwrap();
+    }
+    
+    CURRENT_REGEX.is_match(candidate)
 }
